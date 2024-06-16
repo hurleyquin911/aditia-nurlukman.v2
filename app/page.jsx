078a83a37f1128b63b05f6078b6bc84df1fdde82
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-17 04:32:24 +0700

    Perubahan ketiga

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <div className="grid  bg-white text-black rounded-md">
         Hallo ini ujang dong
+      </div>
+
+      <div>
+        Apakah Manusia disini baik-baik saja ? 
       </div>
     </main>
   );
