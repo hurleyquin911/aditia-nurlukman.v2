ec9c3f89c257df11720a8a4718940fcc565a7c7e
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-17 13:51:27 +0700

    bagian homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import "./globals.css";
 
 //
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -20,7 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={JetbrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
